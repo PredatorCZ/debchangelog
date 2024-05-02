f9c3e58320baa2807b8b7fa90fcc4fdc9ed07261
Radek Smejdir <radek.smejdir@firma.seznam.cz>
2024-05-02 07:55:45 +0200

    fix: installation on Debian Bookworm

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='dchmerge',
-    version='0.1.0',
+    version='0.1.1',
     description='Merge conflict resolver for debian changelogs',
     author='Lukas Cone',
     url='https://github.com/PredatorCZ/debchangelog',
-    scripts=['dchmerge.py', 'deb.py']
+    scripts=['dchmerge.py', 'deb.py'],
+    py_modules=[]
 )
